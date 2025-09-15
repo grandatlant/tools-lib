86eacc77212a98d921af3bc5b64812b24161cbc5
grandatlant <grandatlant@gmail.com>
2025-09-15 15:25:00 +0300

    Submodule cleanup, formatting. pyproject update.

---
+++ tpl.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S python3 -O
+# -*- coding: utf-8 -*-
+# /// script
+# requires-python = ">=3.8"
+# dependencies = [
+# ]
+# ///
+"""Doc"""
+
+import sys
+
+
+##  MAIN ENTRY POINT
+def main(args=None):
+    args = sys.argv if args is None else args
+    assert isinstance(args, (list, tuple))
+    print(args)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
